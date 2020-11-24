4fda468d6ca9a0d1f4648c2936e3027165afb8eb
Dilanga Algama <31867952+Dalga082@users.noreply.github.com>
2020-11-24 09:39:36 -0500

    Update Chain.cpp
    removed a print line.

--- Chain.cpp
+++ Chain.cpp
@@ -14,7 +14,6 @@ template<typename T>
 Chain<T>& Chain<T>::operator+=(Card* card) {
     if (typeid(c) != typeid(chain_cards.front())) {
         throw new IllegalType("Not the same type.");
-        cout << "Authentic exception." << endl;
     }
     else {
         chain_cards.push_back(card);
