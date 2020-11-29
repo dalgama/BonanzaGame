506aa91872aefbffa45420a7726db7190ee669da
Xiaoxi Jia <60756026+1998Charles@users.noreply.github.com>
2020-11-28 22:01:48 -0500

    revised

--- Table.cpp
+++ Table.cpp
@@ -1,26 +1,5 @@
 #include "Table.h"
 
-/*
-Table::Table(std::istream& file) {
-	d_deck = CardFactory::getFactory(file)->getDeck();
-	d_discardPile = new DiscardPile(file, CardFactory::getFactory(file));
-	d_tradeArea = new TradeArea(file, CardFactory::getFactory(file));
-	d_players.push_back(new Player(file, getName(1)));
-	d_players.push_back(new Player(file, getName(2)));
-	for (auto const& player : d_players) {
-		for (int i = 0; i < 5; i++) {
-			*player->getHand() += d_deck->draw();
-		}
-	}
-	Player A;
-	Player B;
-	Deck deck;
-	DiscardPile dPile;
-	TradeArea tArea;
-}
-*/
-
-
 Table::Table(istream& in, const CardFactory* set) {
 	A = new Player(in, set);
 	B = new Player(in, set);
